3e4bc75b8cf01a9d37282607449c4f31ebd83f69
perdomofranklindev <perdomofranklindev@gmail.com>
2022-12-26 17:26:52 -0400

    Test

--- init-setup.sh
+++ init-setup.sh
@@ -31,8 +31,8 @@ PASSWORD=$1
 echo -e "Docker 🐳"
 
 echo $PASSWORD | sudo snap install docker 
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+echo $PASSWORD | sudo apt update
+echo $PASSWORD | sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
