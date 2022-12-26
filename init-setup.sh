1dec0c3ac2eea9c7eb490472a18e86a09be8dcb5
perdomofranklindev <perdomofranklindev@gmail.com>
2022-12-26 16:25:25 -0400

    Test

--- init-setup.sh
+++ init-setup.sh
@@ -42,7 +42,7 @@ sudo apt-get install \
 
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg 
-sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg \ yes
+y | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 
 # echo \
