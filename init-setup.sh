1bcca5fdd2575a7c6fe6926debe3552cd5793ee3
perdomofranklindev <perdomofranklindev@gmail.com>
2022-12-26 17:04:20 -0400

    Test

--- init-setup.sh
+++ init-setup.sh
@@ -30,26 +30,7 @@ sudo apt-get install git
 # Docker
 echo -e "Docker 🐳"
 
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-
-sudo rm -rf /etc/apt/keyrings
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg
-sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo $PASSWORD | sudo apt-get update
-yes | sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+echo $PASSWORD | sudo snap install docker 
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
