d3fd259470c1f33a7bdf4ab046a279c4b5428fe8
perdomofranklindev <perdomofranklindev@gmail.com>
2022-12-26 16:57:58 -0400

    Test

--- init-setup.sh
+++ init-setup.sh
@@ -47,33 +47,15 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
-
-# sudo chmod a+r /etc/apt/keyrings/docker.gpg
-# sudo apt-get update
-# sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-
-# ==================
-
-# yes | sudo apt-get remove docker docker-engine docker.io containerd runc
-# yes | sudo apt-get update
-# yes | sudo apt-get install \
-#            ca-certificates \
-#            curl \
-#            gnupg \
-#            lsb-release
-
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg 
-# yes | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-# # yes | echo \
-# #   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-# #   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo $PASSWORD | sudo apt-get update
+yes | sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 # echo $PASSWORD | sudo -S apt-get update
 # yes | sudo apt-get install docker-ce docker-ce-cli containerd.io docker.io
-# sudo groupadd docker
-# sudo usermod -aG docker $USER
-# sudo systemctl enable docker
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
 
 # # Docker Compose.
 # echo -e "Docker Compose 🐳"
