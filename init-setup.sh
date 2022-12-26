5d4e5e6cf1b1600a348a3df3b2e311aa01398f1e
perdomofranklindev <perdomofranklindev@gmail.com>
2022-12-26 16:02:30 -0400

    Test

--- init-setup.sh
+++ init-setup.sh
@@ -40,8 +40,8 @@ sudo apt-get install \
     gnupg \
     lsb-release
 
-# sudo mkdir -p /etc/apt/keyrings
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 # echo \
 #   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
