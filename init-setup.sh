776f19e36d00894c24b157ab2688b76b3c3ea0aa
perdomofranklindev <perdomofranklindev@gmail.com>
2022-12-26 16:43:15 -0400

    Test

--- init-setup.sh
+++ init-setup.sh
@@ -40,21 +40,15 @@ sudo apt-get install \
 
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg
-if [[ -f /etc/apt/keyrings/docker.gpg ]] then
+if ! [[ -f /etc/apt/keyrings/docker.gpg ]] then
   sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 fi
-# if [ -f /etc/apt/keyrings/docker.gpg ]; then
-#   # sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-#   echo "file exists"
-# else
-#   echo "no exists"
-# if
 
-# echo \
-#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-#   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# sudo apt-get update
+sudo apt-get update
 
 # sudo chmod a+r /etc/apt/keyrings/docker.gpg
 # sudo apt-get update
