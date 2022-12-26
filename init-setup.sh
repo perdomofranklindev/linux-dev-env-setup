5bde87ec0142e4db2f4bd5f8b7e1116bb879f487
perdomofranklindev <perdomofranklindev@gmail.com>
2022-12-26 16:00:58 -0400

    Test

--- init-setup.sh
+++ init-setup.sh
@@ -31,19 +31,41 @@ sleep 2s
 # Docker
 echo -e "Docker 🐳"
 sleep 2s
-yes | sudo apt-get remove docker docker-engine docker.io containerd runc
-yes | sudo apt-get update
-yes | sudo apt-get install \
-           ca-certificates \
-           curl \
-           gnupg \
-           lsb-release
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# yes | sudo apt-get remove docker docker-engine docker.io containerd runc
+# yes | sudo apt-get update
+# yes | sudo apt-get install \
+#            ca-certificates \
+#            curl \
+#            gnupg \
+#            lsb-release
 
 # curl -fsSL https://download.docker.com/linux/ubuntu/gpg 
 # yes | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-# yes | echo \
-#   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-#   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# # yes | echo \
+# #   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+# #   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # echo $PASSWORD | sudo -S apt-get update
 # yes | sudo apt-get install docker-ce docker-ce-cli containerd.io docker.io
