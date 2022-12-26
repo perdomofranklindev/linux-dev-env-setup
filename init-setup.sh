c30334ded4aaa0334111b10d3bef842393d036c5
perdomofranklindev <perdomofranklindev@gmail.com>
2022-12-26 14:48:25 -0400

    Test

--- init-setup.sh
+++ init-setup.sh
@@ -18,89 +18,89 @@ echo $PASSWORD | sudo -S apt-get update
 echo $PASSWORD | sudo -S apt-get upgrade
 echo "✔️ System updated successfully!"
 
-# Snap
-echo "Snap 🐦"
-sleep 2s
-sudo apt install snapd
+# # Snap
+# echo "Snap 🐦"
+# sleep 2s
+# sudo apt install snapd
 
-# Git
-echo -e "Git 👨‍💻"
-sleep 2s
-sudo apt-get install git
+# # Git
+# echo -e "Git 👨‍💻"
+# sleep 2s
+# sudo apt-get install git
 
-# Docker
-echo -e "Docker 🐳"
-sleep 2s
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+# # Docker
+# echo -e "Docker 🐳"
+# sleep 2s
+# sudo apt-get remove docker docker-engine docker.io containerd runc
+# sudo apt-get update
+# sudo apt-get install \
+#     ca-certificates \
+#     curl \
+#     gnupg \
+#     lsb-release
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+# echo \
+#   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-echo $PASSWORD | sudo -S apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker.io
-sudo groupadd docker
-sudo usermod -aG docker $USER
-sudo systemctl enable docker
+# echo $PASSWORD | sudo -S apt-get update
+# sudo apt-get install docker-ce docker-ce-cli containerd.io docker.io
+# sudo groupadd docker
+# sudo usermod -aG docker $USER
+# sudo systemctl enable docker
 
-# Docker Compose.
-echo -e "Docker Compose 🐳"
-sleep 2s
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
-sudo chmod +x /usr/bin/docker-compose
+# # Docker Compose.
+# echo -e "Docker Compose 🐳"
+# sleep 2s
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+# sudo chmod +x /usr/bin/docker-compose
 
-# Google Chrome
-echo -e "🔎 Installing Google Chrome..."
-sleep 2s
-yes | sudo apt-get install libxss1 libappindicator1 libindicator7
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-rm -rf google-chrome-stable_current_amd64.deb
+# # Google Chrome
+# echo -e "🔎 Installing Google Chrome..."
+# sleep 2s
+# yes | sudo apt-get install libxss1 libappindicator1 libindicator7
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# sudo dpkg -i google-chrome-stable_current_amd64.deb
+# rm -rf google-chrome-stable_current_amd64.deb
 
-# Visual Studio Code
-echo -e "💻 Installing Visual Studio Code..."
-sleep 2s
-sudo snap install --classic code
+# # Visual Studio Code
+# echo -e "💻 Installing Visual Studio Code..."
+# sleep 2s
+# sudo snap install --classic code
  
-# Android Studio
-echo -e "📱 Installing Android Studio..."
-sleep 2s
-yes | sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
-sudo snap install android-studio --classic
+# # Android Studio
+# echo -e "📱 Installing Android Studio..."
+# sleep 2s
+# yes | sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
+# sudo snap install android-studio --classic
  
-# Postman
-echo -e "📦 Installing Postman..."
-sleep 2s
-sudo snap install postman
+# # Postman
+# echo -e "📦 Installing Postman..."
+# sleep 2s
+# sudo snap install postman
 
-# Spotify
-echo -e "🎧 Installing Spotify..."
-sleep 2s
-sudo snap install spotify
+# # Spotify
+# echo -e "🎧 Installing Spotify..."
+# sleep 2s
+# sudo snap install spotify
 
-# NGROK
-echo -e "🔗 Installing NGROK..."
-sleep 2s
-sudo apt-get install zip unzip
-sudo wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-sudo unzip ngrok-stable-linux-amd64.zip
-sudo chmod +x ngrok
-sudo mv ngrok /usr/bin/
-rm -rf ngrok-stable-linux-amd64.zip
+# # NGROK
+# echo -e "🔗 Installing NGROK..."
+# sleep 2s
+# sudo apt-get install zip unzip
+# sudo wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+# sudo unzip ngrok-stable-linux-amd64.zip
+# sudo chmod +x ngrok
+# sudo mv ngrok /usr/bin/
+# rm -rf ngrok-stable-linux-amd64.zip
 
-# Slack
-echo -e "🎙 Installing Slack..."
-sleep 2s
-sudo snap install slack --classic
+# # Slack
+# echo -e "🎙 Installing Slack..."
+# sleep 2s
+# sudo snap install slack --classic
 
-# Discord
-echo -e "💬 Installing Discord..."
-sleep 2s
-sudo snap install discord
+# # Discord
+# echo -e "💬 Installing Discord..."
+# sleep 2s
+# sudo snap install discord
