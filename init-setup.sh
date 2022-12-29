af02bc3a192e2053cbca54e38e410889de9ddabf
perdomofranklindev <perdomofranklindev@gmail.com>
2022-12-28 21:08:54 -0400

    Fish terminal commands to test

--- init-setup.sh
+++ init-setup.sh
@@ -24,38 +24,38 @@ echo "💻 Setting up the environment..."
 # echo -e "Git 👨‍💻"
 # yes | sudo apt-get install git
 
-# Docker
-echo -e "Docker 🐳"
+# # Docker
+# echo -e "Docker 🐳"
 
-echo $PASSWORD | sudo snap install docker 
-echo $PASSWORD | sudo apt update
-echo $PASSWORD | sudo apt install apt-transport-https ca-certificates curl software-properties-common
+# echo $PASSWORD | sudo snap install docker 
+# echo $PASSWORD | sudo apt update
+# echo $PASSWORD | sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-yes | sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-sudo apt-get update
-yes | sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+# sudo apt-get remove docker docker-engine docker.io containerd runc
+# sudo apt-get update
+# yes | sudo apt-get install \
+#     ca-certificates \
+#     curl \
+#     gnupg \
+#     lsb-release
+# sudo mkdir -p /etc/apt/keyrings
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+# echo \
+#   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+#   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# sudo apt-get update
+# sudo chmod a+r /etc/apt/keyrings/docker.gpg
+# sudo apt-get update
+# yes | sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-sudo usermod -a -G docker $USER
-sudo chmod 777 /var/run/docker.sock
+# sudo usermod -a -G docker $USER
+# sudo chmod 777 /var/run/docker.sock
 
-# Docker Compose.
-echo -e "Docker Compose 🐳"
-sleep 2s
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
-sudo chmod +x /usr/bin/docker-compose
+# # Docker Compose.
+# echo -e "Docker Compose 🐳"
+# sleep 2s
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+# sudo chmod +x /usr/bin/docker-compose
 
 # # Google Chrome
 # echo -e "🔎 Installing Google Chrome..."
@@ -90,3 +90,26 @@ sudo chmod +x /usr/bin/docker-compose
 # echo -e "💬 Installing Discord..."
 # sleep 2s
 # sudo snap install discord
+
+# Fish terminal
+echo "Installing fish terminal..."
+echo -ne '\n' | sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update
+yes | sudo apt install fish
+
+# Fish as default terminal
+echo "configure the default terminal"
+echo /usr/local/bin/fish | sudo tee -a /etc/shells
+echo $PASSWORD | chsh -s $(which fish) # To revert => chsh -s $(which bash)
+
+# Fish Node Version Manager
+echo "Installing Node Version Manager"
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+fisher install jorgebucaran/nvm.fish
+
+# Install Oh my fish
+echo "Installing Oh my fish"
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+echo "\n You can configure your shell theme with https://github.com/oh-my-fish/oh-my-fish/blob/master/docs/Themes.md#agnoster"
+echo "It's super easy to use it!"
