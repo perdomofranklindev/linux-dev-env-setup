02f807af08f0ea79636984689b15c19540322703
Franklin Perdomo <92344582+perdomofranklindev@users.noreply.github.com>
2023-12-16 19:23:45 -0400

    Update optional_tools.sh

--- optional_tools.sh
+++ optional_tools.sh
@@ -126,9 +126,11 @@ function install_fish_terminal() {
     # Fish as default terminal
     echo -e "configure the default terminal 🔨"
     sleep 2s
-    echo /usr/local/bin/fish | sudo tee -a /etc/shells
+    { echo $PASSWORD; echo "/usr/local/bin/fish"; } | sudo -S tee -a /etc/shells
+
     echo $PASSWORD | chsh -s $(which fish) # To revert => chsh -s $(which bash)
 
+    # This for prevent a premature exit
     trap 'echo "Finished installing fish!"' EXIT
     output=$(install_omf 2>&1)
     if [[ $? -eq 0 ]]; then
