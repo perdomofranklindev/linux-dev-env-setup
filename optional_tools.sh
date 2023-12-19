f8b7279aef11def3c93d6427c69d70e785212eb1
Franklin Perdomo <92344582+perdomofranklindev@users.noreply.github.com>
2023-12-18 22:35:17 -0400

    Update optional_tools.sh

--- optional_tools.sh
+++ optional_tools.sh
@@ -156,5 +156,5 @@ function install_pgadmin() {
     sleep 2s
     curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /usr/share/keyrings/packages-pgadmin-org.gpg
     sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
-    sudo apt install pgadmin4
+    yes | sudo apt install pgadmin4
 }
