4b83c00054259a920b6bc5a42a695deda35c9198
perdomofranklindev <perdomofranklindev@gmail.com>
2024-05-25 23:27:05 -0400

    Fish terminal

--- install_omf.sh
+++ install_omf.sh
@@ -3,4 +3,4 @@ echo -e "Installing Oh my fish 🎨"
 sleep 2s
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install >install
 echo -e "Curl oh my fish installation done!"
-fish install --path=~/.local/share/omf --config=~/.config/omf    
+fish install --path=~/.local/share/omf --config=~/.config/omf
